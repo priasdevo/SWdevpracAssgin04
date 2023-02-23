3dd272612179d816cbb49a84fb32f1773c36378c
Naphat <priasdevo@gmail.com>
2023-02-23 13:33:53 +0700

    feat: assignment 6

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser=require('cookie-parser');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -9,10 +10,14 @@ dotenv.config({path:'./config/config.env'});
 connectDB();
 
 const hospitals = require('./routes/hospital');
+const auth = require('./routes/auth');
+
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/auth',auth);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log("Server running in", process.env.NOED_ENV, ' mode on port ', PORT));
