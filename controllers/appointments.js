d92912923d76a07db0295c905b6f9b5eb96e06c9
Naphat <priasdevo@gmail.com>
2023-03-16 19:22:06 +0700

    feat: add ownerShip check // Done with assignment7

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -65,6 +65,14 @@ exports.addAppointment=async (req,res,next) => {
             return res.status(404).json({success:false,message:`No hospital with the id of ${req.params.hospitalId}`})
         }
 
+        req.body.user=req.user.id
+
+        const existedAppointments = await Appointment.find({user:req.user.id});
+
+        if(existedAppointments.length >= 3 && req.user.role !== 'admin'){
+            return res.status(400).json({success:false,message:`The user with ID ${req.user.id} has already made 3 appointments`})
+        }
+
         const appointment = await Appointment.create(req.body);
 
         res.status(200).json({success:true,data: appointment})
@@ -83,6 +91,10 @@ exports.updateAppointment=async (req,res,next) => {
 
         if(!appointment){
             return res.status(404).json({success:false,message:`No appointment with the id of ${req.params.id}`})
+        }
+
+        if(appointment.user.toString()!== req.user.id && req.user.role !== 'admin'){
+            return res.status(401).json({success:false,message:`User ${req.user.id} is not authorized to update this appointment`})
         }
 
         appointment = await Appointment.findByIdAndUpdate(req.params.id,req.body,{
@@ -105,6 +117,9 @@ exports.deleteAppointment=async (req,res,next) => {
 
         if(!appointment){
             return res.status(404).json({success:false,message:`No appointment with the id of ${req.params.id}`})
+        }
+        if(appointment.user.toString()!== req.user.id && req.user.role !== 'admin'){
+            return res.status(401).json({success:false,message:`User ${req.user.id} is not authorized to delete this appointment`})
         }
 
         await appointment.remove();
