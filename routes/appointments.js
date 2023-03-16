27804f6846a270e2fdc135152f5c16f600e0bbbb
Naphat <priasdevo@gmail.com>
2023-03-16 19:12:59 +0700

    feat: add protect/authorize

--- routes/appointments.js
+++ routes/appointments.js
@@ -3,9 +3,9 @@ const {getAppointments, getAppointment, addAppointment, updateAppointment, delet
 
 const router = express.Router({mergeParams:true})
 
-const {protect} = require('../middleware/auth');
+const {protect,authorize} = require('../middleware/auth');
 
-router.route('/').get(protect,getAppointments).post(addAppointment);
-router.route('/:id').get(protect,getAppointment).put(updateAppointment).delete(deleteAppointment);
+router.route('/').get(protect,getAppointments).post(protect,authorize("admin","user"),addAppointment);
+router.route('/:id').get(protect,getAppointment).put(protect,authorize("admin","user"),updateAppointment).delete(protect,authorize("admin","user"),deleteAppointment);
 
 module.exports=router;
